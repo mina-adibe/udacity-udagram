b83fa6925542ecfa3116f0c320d80c9d2043ef57
mina-v <mina.bessada@vodafone.com>
2023-04-24 20:34:05 +0200

    lint fix

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   appName: `Udagram`,
   apiHost:
-    "http://udagram-api-dev.eba-nrdnxjqk.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://udagram-api-dev.eba-nrdnxjqk.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
